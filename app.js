c0d9dd4458811bc8a2c8d95912e5c8f6f52a5979
Fahad Qazi <fahadqazi@gmail.com>
2017-05-31 23:25:28 +0100

    add command line args functionality

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const _ = require('lodash');
 
 const notes = require('./notes.js');
 
+// console.log(process.argv);  //argv = arguments vector
+
+var command = process.argv[2];
+
+if (command === 'add'){
+    console.log('Adding new note')
+} else if (command === 'list'){
+    console.log('Listing all notes')
+} else if (command === 'read'){
+    console.log('Reading note')
+} else if (command === 'remove'){
+    console.log('Removing note')
+} else {
+    console.log('Cgiommand not found');
+}
