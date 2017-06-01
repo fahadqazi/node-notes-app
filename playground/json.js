5c0670d495f860fd8f662a99fe338f03f9293738
Fahad Qazi <fahadqazi@gmail.com>
2017-06-01 23:35:08 +0100

    read from and write to a json file

--- playground/json.js
+++ playground/json.js
@@ -11,3 +11,22 @@
 // var result = JSON.parse(personString);
 // console.log(result);
 // console.log(typeof result);
+
+const fs = require('fs');
+
+var orginalNote = {
+    title: 'Some title',
+    body: 'Some Body'
+};
+
+var orginalNoteString = JSON.stringify(orginalNote);
+
+fs.writeFileSync('notes.json', orginalNoteString);
+
+console.log("Written to file....");
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString)
+
+console.log(note.title);
+
