4314bbb86b7f5b21d7471633d5815d63c5fc0d08
Fahad Qazi <fahadqazi@gmail.com>
2017-06-01 22:32:04 +0100

    add note add remove list get funcs

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-// console.log(process.argv);  //argv = arguments vector
-
+const argv = yargs.argv;
 var command = process.argv[2];
 
+// console.log('Process: ', process.argv);  //argv = arguments vector
+console.log('Yargs: ', argv);
+
+
+
 if (command === 'add'){
-    console.log('Adding new note')
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list'){
-    console.log('Listing all notes')
+    notes.getAll();
 } else if (command === 'read'){
-    console.log('Reading note')
+    notes.getNote(argv.title);
 } else if (command === 'remove'){
-    console.log('Removing note')
+    notes.removeNote(argv.title);
 } else {
-    console.log('Cgiommand not found');
+    console.log('Command not found');
 }
